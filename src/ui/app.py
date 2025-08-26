3a621dd419479635cd07c8435cc47b270bea3e3a
Armaan Sharma <sharmaarmaan.9871@gmail.com>
2025-08-26 15:07:46 +0530

    Implemented the GPS feature Now location can be given by both typed input and gps

--- src/ui/app.py
+++ src/ui/app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import requests
+import streamlit.components.v1 as components
 
 st.title("AI Health Navigator")
 query = st.text_input("Your Question")
@@ -11,7 +12,29 @@ lat,lon=None,None
 
 if use_gps:
     st.markdown("open in browser with location access") #need to implement JS for this (later)
+    gps_html="""
+    <script>
+    navigator.geolocation.getCurrentPosition(
+        function(position){
+            const lat=position.coords.latitude;
+            const lon=position.coords.longitude;
+            document.querySelector('body').innerHTML +=
+            `<input type='hidden' id='lat' value='${lat}'>
+            <input type='hidden' id='lon' value='${lon}'>`;
+        },
+        function(error){alert ('GPS access denied or not available');}
+    );
+    </script>
+    """
+    components.html(gps_html)
 
+    js_lat=st.query_params.get("lat")
+    js_lon=st.query_params.get("lon")
+    if js_lat and js_lon:
+        lat=float(js_lat[0])
+        lon=float(js_lon[0])
+    else:
+        st.info("Please allow GPS access and reload if necessary.")
 if st.button("Ask"):
     payload={
         "question":query,
